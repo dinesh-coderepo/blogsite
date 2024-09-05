afde809c778bcc1db0fb97292ce3f7a43b2e0e9f
Sai Dinesh Reddy Maluchuru <maluchurudinesh@gmail.com>
2024-09-05 20:27:54 +0530

    moved the loadenv

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, redirect, url_for, request, render_template, session
 import requests, os, uuid, json
 from dotenv import load_dotenv
-load_dotenv()
-
 
 app = Flask(__name__)
 
