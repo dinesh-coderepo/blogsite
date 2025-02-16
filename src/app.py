6399f483da09b510d388e51d8f1ae935dd47be9c
Maluchuru Sai Dinesh Reddy <maluchurudinesh@gmail.com>
2025-02-16 09:57:33 +0530

    [Design]

--- src/app.py
+++ src/app.py
@@ -167,5 +167,6 @@ def blog_image(folder, filename):
 def about():
     return render_template('about.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
