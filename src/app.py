fda3c283b28e80cb68735f99d2ea1b001c860ffa
Maluchuru Sai Dinesh Reddy <maluchurudinesh@gmail.com>
2025-02-22 23:28:00 +0530

    [Design] added design changes

--- src/app.py
+++ src/app.py
@@ -54,59 +54,15 @@ class MermaidExtension(Extension):
     def extendMarkdown(self, md):
         md.preprocessors.register(MermaidPreprocessor(md), 'mermaid', 175)
 
-@app.route('/translator',methods=['GET'])
-def index():
-    return render_template('index_translator.html')
+# Remove translator routes and related functions:
+# @app.route('/translator', methods=['GET'])
+# def index():
+#     return render_template('index_translator.html')
 
-
-@app.route('/translator', methods=['POST'])
-def index_post():
-    # Read the values from the form
-    original_text = request.form['text']
-    target_language = request.form['language']
-
-
-    # Load environment variables from .env file if running locally
-    load_dotenv()  # Only loads if .env file exists
-    key = os.getenv('KEY')
-    endpoint = os.getenv('ENDPOINT')
-    location = os.getenv('LOCATION')
-
-
-
-    # Indicate that we want to translate and the API version (3.0) and the target language
-    path = '/translate?api-version=3.0'
-    # Add the target language parameter
-    target_language_parameter = '&to=' + target_language
-    # Create the full URL
-    constructed_url = endpoint + path + target_language_parameter
-
-    # Set up the header information, which includes our subscription key
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
-
-    # Create the body of the request with the text to be translated
-    body = [{ 'text': original_text }]
-
-    # Make the call using post
-    translator_request = requests.post(constructed_url, headers=headers, json=body)
-    # Retrieve the JSON response
-    translator_response = translator_request.json()
-    # Retrieve the translation
-    translated_text = translator_response[0]['translations'][0]['text']
-
-    # Call render template, passing the translated text,
-    # original text, and target language to the template
-    return render_template(
-        'results_translator.html',
-        translated_text=translated_text,
-        original_text=original_text,
-        target_language=target_language
-    )
+# @app.route('/translator', methods=['POST'])
+# def index_post():
+#     # ...translator POST logic...
+#     return render_template('results_translator.html', ...)
 
 def load_blog_posts():
     posts = []
